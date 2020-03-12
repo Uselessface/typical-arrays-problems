88ea4c3c6fccdef919cf7095f7914f4ba6bbc3bc
Uselessface <60576572+Uselessface@users.noreply.github.com>
2020-03-12 23:51:12 +0400

    solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (a) {
+	return a instanceof Array && a.length > 0 ? Math.min(...a) : 0;
+}
+	
+exports.max = function max (ar) {
+  	return ar instanceof Array && ar.length > 0 ? Math.max(...ar) : 0;
 }
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (arr) {
+  	return arr instanceof Array && arr.length > 0 ? arr.reduce((a, b) => (a + b), 0) / arr.length : 0;
 }
